1b097782101c8a637442118d1952f919eb053f9c
acer <eswarsingh49@gmail.com>
2026-05-19 15:35:13 +0530

    chore: remove unused desktop-app folder

--- desktop-app/preload.js
+++
@@ -1,5 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  saveServerUrl: (url) => ipcRenderer.send('save-server-url', url)
-});
